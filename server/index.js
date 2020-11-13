cfbf0d85fa1454ca64226e17951c2eae4f0cf0e4
khanguslee <khanguslee@gmail.com>
2020-11-13 22:26:31 +1100

    feat: Generate simple nodejs backend

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+// Add middleware to parse requests
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Host the build folder of the client application
+app.use(express.static('../client/build'));
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}.`);
+  });
+  
