0477aa631e315fb3f99492bfa4204c08894f3266
Angus Lee <angus.lee809@gmail.com>
2020-11-14 00:16:02 +1100

    feat: Setup logging for backend (#10)

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const logger = require('./logger');
+
 const app = express();
 
 const PORT = process.env.PORT || 5000;
@@ -12,5 +14,5 @@ app.use(express.json());
 app.use(express.static('../client/build'));
 
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}.`);
+  logger.info(`Server listening on port ${PORT}.`);
 });
